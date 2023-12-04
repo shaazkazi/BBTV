763e86f2b10c3799116ac73f4ac0e1f36f01277a
Mohammed Shariq Kazi <shariqkazi201@gmail.com>
2023-12-04 16:01:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(queryString);
     const movieId = urlParams.get('movie');
     
-    // Use the movieId to dynamically set the HLS stream URL
-    const hlsStreamURL = getHLSStreamURLForMovie(movieId);
+    // Use the movieId to dynamically set the HLS stream URL and get movie details
+    const { hlsStreamURL, movieDetails } = getHLSStreamURLAndDetailsForMovie(movieId);
 
     const defaultOptions = {};
 
@@ -19,45 +19,105 @@ document.addEventListener('DOMContentLoaded', () => {
             const availableQualities = hls.levels.map((l) => l.height);
 
             // Add new qualities to option
-      defaultOptions.quality = {
-        default: availableQualities[0],
-        options: availableQualities,
-        // this ensures Plyr to use Hls to update quality level
-        forced: true,        
-        onChange: (e) => updateQuality(e),
-      }
+            defaultOptions.quality = {
+                default: availableQualities[0],
+                options: availableQualities,
+                // this ensures Plyr to use Hls to update quality level
+                forced: true,        
+                onChange: (e) => updateQuality(e),
+            }
 
             // Initialize here
-      const player = new Plyr(video, defaultOptions);
-    });
-    hls.attachMedia(video);
-    window.hls = hls;
-  } else {
-    // default options with no quality update in case Hls is not supported
-    const player = new Plyr(video, defaultOptions);
-  }
+            const player = new Plyr(video, defaultOptions);
 
-  function updateQuality(newQuality) {
-    window.hls.levels.forEach((level, levelIndex) => {
-      if (level.height === newQuality) {
-        console.log("Found quality match with " + newQuality);
-        window.hls.currentLevel = levelIndex;
-      }
-    });
-  }
+            // Update movie information on the player page
+            updateMovieInformation(movieDetails);
+        });
+        hls.attachMedia(video);
+        window.hls = hls;
+    } else {
+        // default options with no quality update in case Hls is not supported
+        const player = new Plyr(video, defaultOptions);
+
+        // Update movie information on the player page
+        updateMovieInformation(movieDetails);
+    }
+
+    function updateQuality(newQuality) {
+        window.hls.levels.forEach((level, levelIndex) => {
+            if (level.height === newQuality) {
+                console.log("Found quality match with " + newQuality);
+                window.hls.currentLevel = levelIndex;
+            }
+        });
+    }
+
+    function updateMovieInformation(details) {
+        // Update movie details on the player page
+        document.getElementById('movieTitle').innerText = details.title;
+        document.getElementById('rating').innerText = `Rating: ${details.rating}`;
+        document.getElementById('genre').innerText = `Genre: ${details.genre}`;
+        document.getElementById('duration').innerText = `Duration: ${details.duration}`;
+        document.getElementById('movieDescription').innerText = details.description;
+    }
 });
 
-function getHLSStreamURLForMovie(movieId) {
+function getHLSStreamURLAndDetailsForMovie(movieId) {
     switch (movieId) {
         case 'movie1':
-            return 'https://prod-ent-live-gm.jiocinema.com/hls/live/2105488/hd_akamai_iosmob_avc_24x7_bbhindi_day01/master.m3u8';
+            return {
+                hlsStreamURL: 'https://prod-ent-live-gm.jiocinema.com/hls/live/2105488/hd_akamai_iosmob_avc_24x7_bbhindi_day01/master.m3u8',
+                movieDetails: {
+                    title: 'Bigg Boss Hindi Live 24x7',
+                    rating: '4.5',
+                    genre: 'Reality TV',
+                    duration: '24/7',
+                    description: 'Description for Bigg Boss Hindi Live 24x7.'
+                }
+            };
         case 'movie2':
-            return 'https://prod-ent-live-gm.jiocinema.com/bpk-tv/Colors_HD_voot_MOB/Fallback/index.m3u8';
+            return {
+                hlsStreamURL: 'https://prod-ent-live-gm.jiocinema.com/bpk-tv/Colors_HD_voot_MOB/Fallback/index.m3u8',
+                movieDetails: {
+                    title: 'ColorsTV Hindi HD',
+                    rating: '4.2',
+                    genre: 'Entertainment',
+                    duration: '2h 30min',
+                    description: 'Description for ColorsTV Hindi HD.'
+                }
+            };
         case 'movie3':
-            return 'https://prod-ent-live-gm.jiocinema.com/bpk-tv/Colors_Kannada_HD_voot_MOB/Fallback/index.m3u8';
+            return {
+                hlsStreamURL: 'https://prod-ent-live-gm.jiocinema.com/bpk-tv/Colors_Kannada_HD_voot_MOB/Fallback/index.m3u8',
+                movieDetails: {
+                    title: 'ColorsTV Kannada HD',
+                    rating: '4.0',
+                    genre: 'Entertainment',
+                    duration: '2h 15min',
+                    description: 'Description for ColorsTV Kannada HD.'
+                }
+            };
         case 'movie4':
-            return 'https://prod-ent-live-gm.jiocinema.com/hls/live/2101563/hd_akamai_iosmob_avc_24x7_bbkannada_day01/master.m3u8';
+            return {
+                hlsStreamURL: 'https://prod-ent-live-gm.jiocinema.com/hls/live/2101563/hd_akamai_iosmob_avc_24x7_bbkannada_day01/master.m3u8',
+                movieDetails: {
+                    title: 'Bigg Boss Kannada Live 24x7',
+                    rating: '4.7',
+                    genre: 'Reality TV',
+                    duration: '24/7',
+                    description: 'Description for Bigg Boss Kannada Live 24x7.'
+                }
+            };
         default:
-            return 'https://prod-ent-live-gm.jiocinema.com/hls/live/2105488/hd_akamai_iosmob_avc_24x7_bbhindi_day01/master.m3u8';
+            return {
+                hlsStreamURL: 'https://prod-ent-live-gm.jiocinema.com/hls/live/2105488/hd_akamai_iosmob_avc_24x7_bbhindi_day01/master.m3u8',
+                movieDetails: {
+                    title: 'Bigg Boss Hindi Live 24x7',
+                    rating: '4.5',
+                    genre: 'Reality TV',
+                    duration: '24/7',
+                    description: 'Description for Bigg Boss Hindi Live 24x7.'
+                }
+            };
     }
 }
